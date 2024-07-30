8bc0c705e80baf95ec2a6487af159d764ee3d6ef
bobleau <115632696+bobleau@users.noreply.github.com>
2024-07-31 00:02:09 +0800

    Update debian12init.sh

--- shell/debian12init.sh
+++ shell/debian12init.sh
@@ -4,7 +4,7 @@ timedatectl set-timezone Asia/Shanghai
 echo sleep-inactive-ac-timeout=0 >> /etc/gdm3/greeter.dconf-defaults
 apt-mark manual desktop-base network-manager-gnome gnome-terminal file-roller
 apt purge -y ace-of-penguins aisleriot atomix cheese evolution five-or-more four-in-a-row gbrainy gnome-2048 gnome-calendar gnome-characters gnome-chess gnome-clocks gnome-contacts gnome-klotski gnome-mahjongg gnome-maps gnome-mines gnome-nibbles gnome-robots gnome-sound-recorder gnome-sudoku gnome-sushi gnome-taquin gnome-tetravex gnome-todo gnome-weather hitori iagno libreoffice* lightsoff quadrapassel rhythmbox simple-scan swell-foop tali transmission-gtk yelp
-cat < /etc/apt/sources.list
+cat <<EOF > /etc/apt/sources.list
 deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm main contrib non-free non-free-firmware
 deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-updates main contrib non-free non-free-firmware
 deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bookworm-backports main contrib non-free non-free-firmware
